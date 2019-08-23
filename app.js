8f3321a2b38cf311745b919f7bb9f5eaf1eb3f0e
Sayan Singha <sayansingha@Sayans-MacBook-Pro.local>
2019-08-23 21:06:25 +0530

    Wrapping 2 api calls in a function

--- app.js
+++ app.js
@@ -4,23 +4,30 @@ const geocode = require('./utils/geocode.js')
 
 console.time("TotalTime");
 
-let response = 1
-for(let i = 1; i <= 10; i++)
-{
-    //let responsetimer = i;
-    let a = Date.now()
-    geocode('Raiganj West Bengal', (error, data) => {
-        forecast(data.longitude, data.lattitude, (error, forecastData) => {
-            console.log(data.location)
-            console.log(forecastData.summary)
-            //console.timeEnd("TotalTime")
-            // forecastData.responsetimer = i
-            response++
-            if(response === 11)
-                console.timeEnd("TotalTime")
-        })  
-    
-        
+function getweather(location, cb) {
+  geocode(location, (error, data) => {
+    forecast(data.longitude, data.lattitude, (error, forecastData) => {
+
+
+      if (cb) {
+        cb(forecastData)
+      }
     })
+
+
+  })
+}
+
+let responseCount = 1
+for (let i = 1; i <= 10; i++) {
+  //let responsetimer = i;
+  let a = Date.now()
+  getweather("raiganj west bengal", (forecast) => {
+    console.log(forecast.summary)
+    responseCount++
+    if (responseCount === 11) {
+      console.timeEnd("TotalTime")
+    }
+  })
 }
 console.log('Starts');
