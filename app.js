b88ec8e90a82499e47fc8bde1054059ec5709811
Sayan Singha <sayansingha@Sayans-MacBook-Pro.local>
2019-08-23 16:08:31 +0530

    Total time taken for the entire execution using console.time()

--- app.js
+++ app.js
@@ -1,32 +1,26 @@
-//import request  from "https";
-console.time("Total Time");
 const request = require('request');
 const forecast = require('./utils/forecast.js')
 const geocode = require('./utils/geocode.js')
 
-var i 
-for(i = 1;i < 10;i++){
-  
-geocode('Raiganj West Bengal',(error, data)=>{
+console.time("TotalTime");
+
+let response = 1
+for(let i = 1; i <= 10; i++)
+{
+    //let responsetimer = i;
+    let a = Date.now()
+    geocode('Raiganj West Bengal', (error, data) => {
+        forecast(data.longitude, data.lattitude, (error, forecastData) => {
+            console.log(data.location)
+            console.log(forecastData.summary)
+            //console.timeEnd("TotalTime")
+            // forecastData.responsetimer = i
+            response++
+            if(response === 11)
+                console.timeEnd("TotalTime")
+        })  
     
         
-    forecast(data.longitude,data.lattitude, (error, forecastData) => {
-        //console.log('Error', error)
-        console.log(data.location)
-      console.log(forecastData)
-      })
-     
-})
+    })
 }
-
-console.timeEnd("Total Time");
-
- 
-console.log('Sayan Singha')
-
-
-
-// forecast(-75.7088, 44.1545, (error, data) => {
-//     console.log('Error', error)
-//     console.log('Data', data)
-//   })
+console.log('Starts');
