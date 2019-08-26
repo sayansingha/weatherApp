e3bd4ad47de17ae1079cee11466bcbfd98325baa
Sayan Singha <sayansingha@Sayans-MacBook-Pro.local>
2019-08-26 19:21:14 +0530

    Implemented promise and modified utils/getweather.js in a new branch dev-promise

--- utils/getweather.js
+++ utils/getweather.js
@@ -2,17 +2,30 @@ const forecast = require('./forecast.js')
 const geocode = require('./geocode.js')
 
 function getweather(location, cb) {
-    geocode(location, (error, data) => {
-      forecast(data.longitude, data.lattitude, (error, forecastData) => {
-  
-  
-        if (cb) {
-          cb(forecastData)
-        }
-      })
-  
-  
-    })
-  }
+  geocode(location, (error, data) => {
+    forecast(data.longitude, data.lattitude, (error, forecastData) => {
 
-  module.exports = getweather
+
+      if (cb) {
+        cb(forecastData)
+      }
+    })
+
+
+  })
+}
+const promise = new Promise((resolve, reject) => {
+  geocode("Raiganj", (error, data) => {
+    forecast(data.longitude, data.lattitude, (error, forecastData) => {
+
+      resolve({forecastData,data})
+    })
+  })
+})
+
+promise.then(({forecastData,data}) => {
+  console.log(data.location)
+  console.log(forecastData.summary)
+})
+
+module.exports = getweather
